bc5bc09ea65f81f71d01787d4ed5fdb671f306f5
ArantzaJuan <arantzajuanal@gmail.com>
2022-12-13 19:27:42 -0600

    Add project files.

---
+++ ConsoleApp1/Edades.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Edades
+    {
+        public static void Edad()
+        {
+            int edad,mayor=0,menor=0;
+            for (int i =0; i<=4; i++ )
+            {
+                Console.WriteLine("Ingrese su edad");
+                edad = int.Parse(Console.ReadLine());
+                if(edad>=18)
+                {
+                    mayor += 1;
+                }
+                else
+                {
+                    menor += 1;
+                }
+            }
+            Console.WriteLine("Personas mayores de edad: "+mayor);
+            Console.WriteLine("Personas mayores de edad: " + menor);
+            Console.ReadKey();
+        }
+       
+    }
+}
